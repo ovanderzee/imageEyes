f7bf3bcf73ef8304346e647819e010ac2352f1c7
George Pickering <29524044+geopic@users.noreply.github.com>
2020-05-06 14:50:35 +0100

    Add types declaration file

---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+declare module 'imageEyes' {
+
+  interface EyeDropper {
+    image: string | undefined;
+    getPixelColor: (x: number, y: number) => number[];
+    getDropColor: (x: number, y: number, d: number) => number[];
+    memoryUsage: () => number;
+    purgeCache: () => void;
+  }
+
+  export default function imageEyes(url: string): Promise<EyeDropper>;
+}
